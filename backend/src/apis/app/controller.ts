6db94ebdcafe715b8dc2ca245bfe51e283331f0f
Jerry Chen <ordinary9843@gmail.com>
2024-10-02 00:29:48 +0800

    feat(backend): WIP on fetching google news

--- backend/src/apis/app/controller.ts
+++ backend/src/apis/app/controller.ts
@@ -1,7 +1,5 @@
-import { Controller } from '@nestjs/common';
-import {
-  ApiTags,
-} from '@nestjs/swagger';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { AppService } from './service';
 
@@ -9,4 +7,9 @@ import { AppService } from './service';
 @ApiTags(AppController.name)
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('test')
+  async test(): Promise<any> {
+    return await this.appService.test();
+  }
 }
